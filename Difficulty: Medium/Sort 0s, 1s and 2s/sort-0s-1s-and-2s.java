3a8cc9e3bf4687f5fe85577f8c52d723739b7353
Pasula Laxmi Narasimha Raju <lnraju366@gmail.com>
2026-03-30 21:59:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
@@ -0,0 +1,28 @@
+class Solution {
+    public void sort012(int[] arr) {
+        // code here
+        int low=0;int mid =0;int high = arr.length -1;
+        
+        while(mid <= high)
+        {
+            if(arr[mid] == 0)
+            {
+                arr[mid] = arr[low];
+                arr[low] = 0;
+                low++;
+                mid++;
+            }
+            else if(arr[mid] == 1)
+            {
+                mid++;
+            }
+            else{
+                int temp = arr[mid];
+                arr[mid] = arr[high];
+                arr[high] = temp;
+                
+                high--;
+            }
+        }
+    }
+}
